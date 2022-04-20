be9255413e6c08bade5180db8f900291f0661ff5
Darius Sinha <dsinha@bbns.org>
2022-04-20 18:49:43 -0400

    change config

--- src/scripts/Config.tsx
+++ src/scripts/Config.tsx
@@ -1,7 +1,7 @@
 const config = {
     pusherKey: "77fde6451dc6f79cf99b",
-    corsAnywhere: "",
-    flaskServer: "https://songleflask.ngrok.io",
+    corsAnywhere: "https://songlecors.herokuapp.com/",
+    flaskServer: "https://songleflask.herokuapp.com",
     songleAddress: "https://songle.infinityideas.dev"
 }
 
