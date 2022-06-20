c559e0c92a9828d0c0f24c4c211ffee7a6a23d10
Darius Sinha <dsinha@bbns.org>
2022-06-19 21:31:03 -0400

    Changed songle footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import '../styles/Footer.css';
 import deezerlogo from '../assets/images/deezerlogo.svg';
 
 function Footer() {
-    const insideText = <p>Created by Darius. <span dangerouslySetInnerHTML={{"__html": "&copy;"}} /> 2022. Thanks to <img src={deezerlogo} height="15px" /><br /> Bug/Comment/Suggestion? Let me know <a href="https://forms.gle/7dS5XuEpyo8jJfrA6">here</a>. View system <a href="https://status.infinityideas.dev">status</a> or the <a href="https://github.com/infinityideas/randomsongle">source code</a>.</p>;
+    const insideText = <p>Created by Darius. <span dangerouslySetInnerHTML={{"__html": "&copy;"}} /> 2022. Thanks to <img src={deezerlogo} height="15px" /><br /> Bug/Comment/Suggestion? Let me know <a href="https://forms.gle/7dS5XuEpyo8jJfrA6">here</a>. View system <a href="https://status.infinityideas.dev">status</a> or the <a href="https://github.com/infinityideas/songle">source code</a>.</p>;
 
     return (
         <div>
